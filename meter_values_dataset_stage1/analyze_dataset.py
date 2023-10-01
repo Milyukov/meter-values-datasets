4f641c5b2511a1ca469de21354b853b913135763
Gleb Milyukov <cvresearch.milyukov@gmail.com>
2023-10-01 17:31:12 +0400

    Added script for dtataset analysis

---
+++ meter_values_dataset_stage1/analyze_dataset.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+
+if __name__ == '__main__':
+    (train_dataset, val_dataset, test_dataset), dataset_info = tfds.load(
+        "meter_values_dataset_stage1", split=["train", "validation", "test"], with_info=True, data_dir=".",
+        read_config=tfds.ReadConfig(try_autocache=False)
+    )
+
+    #stat = tfds.show_statistics(dataset_info)
+    # for val in train_dataset:
+    #    pass
+    vals = np.fromiter(train_dataset.map(lambda x: x['objects']['label']), float)
+
+    plt.hist(vals)
+    plt.xticks(range(4))
+    plt.title('Label Frequency')
+    plt.savefig('label_dist.png')
